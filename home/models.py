c36a1d23422b071a1145fb7777bf05f23727e5b4
sivabala21 <sivabalahack@gmail.com>
2024-02-28 12:39:36 +0530

    Database cleansed

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class post(models.Model):
     user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=1000)
+    content = models.TextField(max_length=10000)
     date_posted = models.DateTimeField(auto_now_add=True)
     upvotes = models.IntegerField(default=0)
 
